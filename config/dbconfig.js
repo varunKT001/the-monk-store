3985ad5cd9e29070358d607d31186a4e7cbe1741
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2021-07-06 16:24:31 +0530

    fixed bug

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg')
 
-const isDevelopment = process.env.NODE_ENV ? true:false
+const isDevelopment = (process.env.NODE_ENV == 'development') ? true:false
 
 const connectionString = isDevelopment ? `postgresql://${process.env.DBUSER}:${process.env.DBPASSWORD}@${process.env.DBHOST}:${process.env.DBPORT}/the_monk_store` : process.env.DATABASE_URL
 const sslVal = isDevelopment ? false : true
